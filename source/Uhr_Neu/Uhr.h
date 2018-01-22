d027c51c07688e4c2574bb781c84b09a192fb36f
Lars Weimar <Eisbaeeer@gmail.com>
2018-01-22 20:51:15 +0100

    Version 2.0.8
    WLAN Scan

--- source/Uhr_Neu/Uhr.h
+++ source/Uhr_Neu/Uhr.h
@@ -2,7 +2,7 @@
  * Hier Anpassungen der Hardware vornehmen.
  */
 
-#define VER "2.0.7"  // Software Version
+#define VER "2.0.8"  // Software Version
 
 /*--------------------------------------------------------------------------
  * Hier wird definiert, welche Anzahl von LED´s bzw. Reihen verwendet werden
