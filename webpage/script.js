ESPWortuhr/Multilayout-ESP-Wordclock

commit 7c234a275bf08d2fade82dd5a4b043d78de3205d

Fixed Sprachoption Options not showing on added Uhrtypes on Website.

webpage/script.js
@@ -350,6 +350,8 @@ function initWebsocket() {
 			$("#boot-show-ip").set("checked", data.bootShowIP | 0);
 
 			enableSpecific("specific-layout-2", data.UhrtypeDef === 2);
+			enableSpecific("specific-layout-2", data.UhrtypeDef === 6);
+			enableSpecific("specific-layout-2", data.UhrtypeDef === 7);
 			enableSpecific("specific-layout-4", data.UhrtypeDef === 4);
 			enableSpecific("specific-layout-5", data.UhrtypeDef === 5);
 			enableSpecific("specific-colortype-4", data.colortype === 4);

commit 86b8f2da2efd3352c0680713b7c6e56917876622

correctly show settings for 'dreiviertel'
Commit 7c234a2 introduced a bug, that it only showed this setting for
the new layout, instead of for old and new layouts.

webpage/script.js
@@ -345,9 +345,7 @@ function initWebsocket() {
 			$("#boot-show-wifi").set("checked", data.bootShowWifi | 0);
 			$("#boot-show-ip").set("checked", data.bootShowIP | 0);
 
-			enableSpecific("specific-layout-2", data.UhrtypeDef === 2);
-			enableSpecific("specific-layout-2", data.UhrtypeDef === 6);
-			enableSpecific("specific-layout-2", data.UhrtypeDef === 7);
+			enableSpecific("specific-layout-2", data.UhrtypeDef === 2 || data.UhrtypeDef === 6 || data.UhrtypeDef === 7);
 			enableSpecific("specific-layout-4", data.UhrtypeDef === 4);
 			enableSpecific("specific-layout-5", data.UhrtypeDef === 5);
 			enableSpecific("specific-colortype-4", data.colortype === 4);