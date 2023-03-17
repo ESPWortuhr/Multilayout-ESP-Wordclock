2886296dc5ec73b84f8991e8e02a7602cb2b9e80
Jonas Scherer <jonas@schererhome.de>
2023-03-17 22:03:37 +0100

    More Formatting

--- include/Uhrtypes/DE10x11.vertical.hpp
+++ include/Uhrtypes/DE10x11.vertical.hpp
@@ -5,8 +5,8 @@
 /*
  * Vertical Layout Front
  * Minutes 110 111 112 113
- * 
- * E S K I S T A F Ü N F    009 010 029 030 049 050 069 070 089 090 109 
+ *
+ * E S K I S T A F Ü N F    009 010 029 030 049 050 069 070 089 090 109
  * Z E H N Z W A N Z I G    008 011 028 031 048 051 068 071 088 091 108
  * D R E I V I E R T E L    007 012 027 032 047 052 067 072 087 092 107
  * V O R F U N K N A C H    006 013 026 033 046 053 066 073 086 093 106
@@ -16,11 +16,10 @@
  * S E C H S N L A C H T    002 017 022 037 042 057 062 077 082 097 102
  * S I E B E N Z W Ö L F    001 018 021 038 041 058 061 078 081 098 101
  * Z E H N E U N K U H R    000 019 020 039 040 059 060 079 080 099 100
-*/
+ */
 
 class De10x11Vertical_t : public iUhrType {
-public: 
-
+public:
     //------------------------------------------------------------------------------
 
     virtual LanguageAbbreviation usedLang() override {
@@ -36,14 +35,14 @@ public:
     virtual const void getFrontMatrixColRow(uint8_t &row, uint8_t &col,
                                             const uint16 index) {
         row = index % rowsWordMatrix();
-        col = colsWordMatrix() -1 -(index/rowsWordMatrix());
+        col = colsWordMatrix() - 1 - (index / rowsWordMatrix());
         if (col % 2 == 0) {
             row = rowsWordMatrix() - 1 - row;
         }
     };
 
     //------------------------------------------------------------------------------
-    
+
     void show(FrontWord word) override {
         switch (word) {
 
