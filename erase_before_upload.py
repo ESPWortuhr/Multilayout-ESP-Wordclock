8780ddad651a414a56ca02fdb113a23ab74d02e4
David Panusch <d.panusch@gmx.de>
2026-02-24 10:07:46 +0100

    Added Erase_before_upload script

--- erase_before_upload.py
+++ erase_before_upload.py
@@ -1,11 +1,15 @@
 Import("env")
 
-old_uploaderflags = env["UPLOADERFLAGS"]
-#print("Old uploaderflags: " + str(old_uploaderflags))
-    
-index_write_flash = old_uploaderflags.index("write_flash")
-if index_write_flash != -1: 
-    new_uploaderflags = old_uploaderflags[::]
-    new_uploaderflags.insert(index_write_flash + 1, "--erase-all")
-    #print("Replaced with: " + str(new_uploaderflags))
-    env.Replace(UPLOADERFLAGS=new_uploaderflags)
+uploader_flags = env.get("UPLOADERFLAGS", [])
+
+if "--erase-all" not in uploader_flags:
+    try:
+        index = uploader_flags.index("write_flash")
+        uploader_flags.insert(index + 1, "--erase-all")
+        
+        env.Replace(UPLOADERFLAGS=uploader_flags)
+        print("Successfully added --erase-all to UPLOADERFLAGS")
+    except ValueError:
+
+        env.Append(UPLOADERFLAGS=["--erase-all"])
+        print("Added --erase-all to the end of UPLOADERFLAGS")
