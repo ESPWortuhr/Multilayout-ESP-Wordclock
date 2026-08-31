e77664b5d8f698ef5293b1c00be142f354bdbb9a
David Panusch <d.panusch@gmx.de>
2026-08-31 14:17:03 +0200

    Updatet Lint to v27

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,7 +16,7 @@ module.exports = function(grunt) {
 
 		eslint: {
 			options: {
-				overrideConfigFile: "eslintrc.json"
+				overrideConfigFile: "eslint.config.js"
 			},
 			files: [
 				"Gruntfile.js",
